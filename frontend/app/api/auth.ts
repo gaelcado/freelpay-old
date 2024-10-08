0bd95e83e9b0dd480c06ccc02b35d76b114eb4f3
IllanKnf <illan_knafou@hotmail.fr>
2024-10-08 23:46:34 +0200

    Update auth.ts

--- frontend/app/api/auth.ts
+++ frontend/app/api/auth.ts
@@ -1,7 +1,7 @@
 // app/api/auth.ts
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://freelpay-nextjs-pm2fo.ondigitalocean.app/api';
 
 export const login = async (username: string, password: string) => {
   const formData = new URLSearchParams();
