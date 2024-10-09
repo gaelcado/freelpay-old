c0f06b98075a74a79c7dc4fa8f3403f85e37ebc9
IllanKnf <illan_knafou@hotmail.fr>
2024-10-09 09:29:26 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_API_URL: "https://freelpay.com/api",
   },
 }
 
